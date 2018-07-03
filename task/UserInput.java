3f19df67b0fd8d4ddf0838be25e62565ccebb6c0
nwyee <nanwittyee@gmail.com>
2018-07-03 15:03:08 +0900

    Create UserInput.java

---
+++ task/UserInput.java
@@ -0,0 +1,53 @@
+/**
+User interface contains two types of user input controls: TextInput, which accepts all characters and NumericInput, 
+which accepts only digits.
+
+Implement the class TextInput that contains:
+
+Public method void add(char c) - adds the given character to the current value
+Public method String getValue() - returns the current value
+Implement the class NumericInput that:
+
+Inherits from TextInput
+Overrides the add method so that each non-numeric character is ignored
+For example, the following code should output "10":
+
+TextInput input = new NumericInput();
+input.add('1');
+input.add('a');
+input.add('0');
+System.out.println(input.getValue());
+*/
+public class UserInput {
+    
+public static class TextInput {
+     String input = "";
+     String output = "";
+    public void add(char ch)
+    {
+        this.input = this.input+ch;
+    }
+    public String getValue(){        
+        return this.input;
+    }
+    
+}
+
+public static class NumericInput extends TextInput {
+    public void add(char ch)
+    {        
+        if(Character.isDigit(ch))
+        {
+            super.input = super.input + ch;
+        }
+    }
+    
+}
+  public static void main(String[] args) {
+        TextInput input = new NumericInput();
+        input.add('1');
+        input.add('a');
+        input.add('0');
+        System.out.println(input.getValue());
+    }
+}
