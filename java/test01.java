d75ac072b7c65e11207d186ea3e2a019b8c8a052
Nan Witt Yee <nanwittyee@gmail.com>
2021-07-04 23:39:58 +0900

    Update test01.java
    fix for time out

--- java/test01.java
+++ java/test01.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class test01 {
     public static void main(String[] args) {
         subsets(new int[]{4,25,16,55});
+        printSumSet(new int[]{4,25,16,55}, 4);
     }
     
     static List<List<Integer>> res ;
@@ -41,5 +42,40 @@ public class test01 {
             backtracking(i+1, curr,nums,size);
             curr.remove(curr.size() - 1);
         }
+    }
+    
+    // not timeout
+    public static void printSumSet(int list[], int n) {
+        int count = 0;
+        int sum = 0;
+        for(int i = 0 ; i < n; i++) {
+            sum += list[i];
+        }
+        
+        boolean[][] memo = new boolean[n+1][sum+1];
+        
+        
+        for(int i = 0 ; i <= n; i++) {
+            memo[i][0] = true;
+        }
+        
+        for(int i = 1; i <= n; i++ ) {
+            memo[i][list[i-1]] = true;
+            for(int j = 1; j <= sum ; j++) {
+                if(memo[i-1][j]) {
+                    memo[i][j] = true;
+                    memo[i][j + list[i-1]] = true;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int j = 0 ; j <= sum; j++) {
+            if(memo[n][j]){
+                count++;
+                sb.append(j+"\n");
+            }
+        }
+        System.out.println(count);
+        System.out.println(sb);
     }
 }
